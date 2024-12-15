d188740b654d8aa6262b67397934d687b6e102e5
Abhishek Sunkara <abhisheksunkara24@gmail.com>
2024-12-14 21:39:49 -0600

    bg-img-test

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col
      bg-[#121212]
-      bg-[url('/images/bg4.png')] sm:bg-cover bg-contain bg-no-repeat bg-center bg-fixed
+      bg-[url('/images/bgnew.jpg')] bg-cover bg-no-repeat bg-center bg-fixed
       ">
       <NavigateBar />
       <div className="container mt-24 mx-auto px-12 py-4">
